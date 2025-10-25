7570111ddce91c9557ed0c2094125afc10897899
Nottackelgamer <minecraftidaryan72@gmail.com>
2025-10-25 17:12:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Configurable Image URLs (Update these with your actual URLs if needed)
 const config = {
     welcomeBg: 'Phantom_Guard_wedge.png', // For welcome section background
-    tabBg: 'space.jpeg'          // For tab content background (if needed)
+    tabBg: ''          // For tab content background (if needed)
 };
 
 // Apply backgrounds dynamically
